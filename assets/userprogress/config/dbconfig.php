0a66c477e4ea25044c0a3947097e2b281de77a48
viven12 <vivenrakesh@gmail.com>
2023-06-30 11:17:41 +0530

    First Commit

---
+++ assets/userprogress/config/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$cfg_dbhost = 'localhost';
+$cfg_dbuser = 'abbmodeapp';
+$cfg_dbpassword = '3rPs79BFaKrMYfYt';
+$cfg_database = 'abbchmprmdb';
+
+/*$cfg_dbhost = 'localhost';
+$cfg_dbuser = 'root';
+$cfg_dbpassword = 'Calisto+1!';
+$cfg_database = 'abbchmprmdb';*/
+?>
