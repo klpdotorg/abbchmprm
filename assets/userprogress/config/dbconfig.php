ef7ec937d466920474d9b08237a5f9a355810d97
viven12 <vivenrakesh@gmail.com>
2024-07-10 20:38:27 +0530

    Db port changes

--- assets/userprogress/config/dbconfig.php
+++ assets/userprogress/config/dbconfig.php
@@ -1,13 +1,20 @@
 <?php
 
+$dbHost = getenv('DB_HOST');
+$dbUsername = getenv('DB_USER');
+$dbPassword = getenv('DB_PASS');
+$dbName = getenv('DB_NAME');
+$dbPort = getenv('DB_PORT');
 
-$cfg_dbhost = 'localhost';
-$cfg_dbuser = 'abbmodeapp';
-$cfg_dbpassword = '3rPs79BFaKrMYfYt';
-$cfg_database = 'abbchmprmdb';
+//$cfg_dbhost = 'localhost';
+//$cfg_dbuser = 'root';
+//$cfg_dbpassword = 'root';
+//$cfg_database = 'abbchmprmdb1';
 
-/*$cfg_dbhost = 'localhost';
-$cfg_dbuser = 'root';
-$cfg_dbpassword = 'Calisto+1!';
-$cfg_database = 'abbchmprmdb';*/
+$cfg_dbhost = $dbHost;
+$cfg_dbuser = $dbUsername;
+$cfg_dbpassword = $dbPassword;
+$cfg_database = $dbName;
+$cfg_port = $dbPort;
+
 ?>
